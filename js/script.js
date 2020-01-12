abea933233537391ad610b99b3b77c01dbdab263
nickn10 <nickn10@comcast.net>
2020-01-12 13:50:56 -0600

    add fade animation for landing imgs

--- js/script.js
+++ js/script.js
@@ -2,15 +2,8 @@ const landingText = Array.from(document.querySelectorAll('.landing-text'));
 // let timer;
 let i = 0;
 
-setInterval(() => {
-    if (i < landingText.length) {
-        landingText[i].classList.add('fade-in');
-        i++
-    } else {
-        landingText.map((text) => text.classList.remove('fade-in'));
-        i = 0;
-    }
-}, 1500);
+landingImgsAnimation();
+landingTxtAnimation();
 
 
 // function fadeIn(x) {
@@ -21,7 +14,34 @@ setInterval(() => {
 //     x.classList.remove('fade-in');
 // };
 
-function fadeInText() {
-    text.classList.add('fade-in');
-    i++;
+// function fadeInText() {
+//     text.classList.add('fade-in');
+//     i++;
+// }
+
+// ============= Landing Animations ===========
+
+function landingImgsAnimation() {
+    window.addEventListener("DOMContentLoaded", function (e) {
+
+        const landing = document.getElementById("landing");
+        const fadeComplete = function (e) { landing.appendChild(arr[0]); };
+        const arr = landing.getElementsByTagName("img");
+        for (var i = 0; i < arr.length; i++) {
+            arr[i].addEventListener("animationend", fadeComplete, false);
+        }
+
+    }, false);
+}
+
+function landingTxtAnimation() {
+setInterval(() => {
+    if (i < landingText.length) {
+        landingText[i].classList.add('fade-in');
+        i++
+    } else {
+        landingText.map((text) => text.classList.remove('fade-in'));
+        i = 0;
+    }
+}, 1100);
 }
